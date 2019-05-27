bace6f697f9604a0736feed13cdc5a916b94a7f5
sidhukulbeer07 <sidhukulbeer93@gmail.com>
2019-05-26 23:54:28 -0700

    commiting changes

--- Top_3_Dest.sh
+++ Top_3_Dest.sh
@@ -3,3 +3,4 @@
 cut -d',' -f18 flightdelays.csv | sort | uniq -c | sort -r -n | head -3 |  tee -a Top_3_dest.csv
 
 cat Top_3_dest.csv | csvlook
+Kulbeer Singh
--- first3sfo.sh
+++ first3sfo.sh
@@ -3,3 +3,4 @@
 echo "ArrDelay,Dest" > first3sfo.csv | cut -d',' -f15,17 flightdelays.csv | grep 'SFO' | head -3 >> first3sfo.csv
 
 cat first3sfo.csv | csvlook
+
